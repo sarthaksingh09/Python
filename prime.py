2703c170d67ef44527d78e88db1ec0672ee05ed7
Shruti Singh <sarthak.thb@gmail.com>
2023-08-10 14:23:51 +0530

    prog to find primes

---
+++ prime.py
@@ -0,0 +1,14 @@
+elements = []
+prime = []
+
+elements = (list(map(int,input("enter the elements").split())))
+for i in elements:
+    count=0
+    for j in range(1,i+1):
+        if i%j==0:
+            count=count+1
+    if count==2:
+        prime.append(i)
+print(prime)
+                
+                    
